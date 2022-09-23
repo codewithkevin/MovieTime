fb67079aa0bf016892096aaf8bdd53ccd4ae9d49
codewithkevin <oseikelvin709@gmail.com>
2022-09-23 03:07:40 +0000

    Update

--- screens/Settings.jsx
+++ screens/Settings.jsx
@@ -3,8 +3,13 @@ import React from 'react'
 
 export default function Settings() {
   return (
-    <View>
+    <View className="flex items-center">
+      <View className="bg-blue-400 static w-full h-[300px] items-center rounded-b-2xl">
+        <View className="bg-black w-[50%] h-[50px] absolute rounded-full align-center items-center">
+
+        </View>
+      </View>
       <Text>Settings</Text>
     </View>
-  )
+  );
 }
