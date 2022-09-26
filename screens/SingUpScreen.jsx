e0ed439deed269a5f3139ee6647e7a46832ad7cf
codewithkevin <oseikelvin709@gmail.com>
2022-09-26 23:17:45 +0000

    Styled both sing up and sing in screens

--- screens/SingUpScreen.jsx
+++ screens/SingUpScreen.jsx
@@ -6,14 +6,16 @@ import {
   Image,
   TouchableOpacity,
   KeyboardAvoidingView,
-  TextInput
+  TextInput,
 } from "react-native";
 import Icon from "react-native-vector-icons/FontAwesome";
 import { Input, Button } from "react-native-elements";
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
+
 const auth = getAuth();
-const SignUpScreen = ({ navigation }) => {
+
+export default function SIngUpScreen({ navigation }) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
@@ -38,12 +40,20 @@ const SignUpScreen = ({ navigation }) => {
       setValidationMessage(error.message);
     }
   }
+
   return (
-    <KeyboardAvoidingView style={styles.container}>
-      <View style={{ width: 150, height: 150, alignSelf: "center" }}>
+    <View
+      className="flex-1 h-full w-full bg-white items-center"
+      style={styles.container}
+    >
+      <View className="mt-[140px]">
+        <Text className="font-bold text-[20px]">Welcome to Movietime!</Text>
+        <Text className="text-center">Your movie is ready</Text>
+      </View>
+      <View className="mt-10 w-[80%]">
         <Input
           placeholder="Email"
-          containerStyle={{ marginTop: 10 }}
+          containerStyle={{ marginTop: 10, backgroundColor: "white" }}
           value={email}
           onChangeText={(text) => setEmail(text)}
           leftIcon={<Icon name="envelope" size={16} />}
@@ -52,8 +62,8 @@ const SignUpScreen = ({ navigation }) => {
           placeholder="Password"
           containerStyle={{ marginTop: 10 }}
           value={password}
-          onChangeText={(value) => validateAndSet(value, setPassword)}
-          secureTextEntry
+          onChangeText={(text) => setPassword(text)}
+          secureTextEntry={true}
           leftIcon={<Icon name="key" size={16} />}
         />
         <Input
@@ -65,41 +75,62 @@ const SignUpScreen = ({ navigation }) => {
           leftIcon={<Icon name="key" size={16} />}
           onBlur={() => checkPassword(password, confirmPassword)}
         />
-        {<Text style={styles.error}>{validationMessage}</Text>}
-        <Button
-          title="Sign up"
-          buttonStyle={{ marginTop: 10 }}
+        <TouchableOpacity>
+          <Text className="text-blue-300">Forgot your password?</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
           onPress={createAccount}
-        />
-        <View>
-          <Text style={{ marginTop: 5, fontSize: 17 }}>
-            Already have an account?
-            <TouchableOpacity
-              onPress={() => navigation.navigate("signin")}
-              style={{ color: "blue", marginLeft: 10 }}
-            >
-              <Text>Login here </Text>
-            </TouchableOpacity>
+          className="items-center mt-10 bg-[#FB5558] p-5 rounded-[30px]"
+        >
+          <Text className="text-white font-bold text-[15px]">SIGN UP</Text>
+        </TouchableOpacity>
+        {
+          <Text className="text-center" style={styles.error}>
+            {validationMessage}
           </Text>
+        }
+
+        <View className="mt-10">
+          <Text className="text-sm text-center text-gray-400 font-bold">
+            or continue with
+          </Text>
+          <View className="flex justify-between flex-row">
+            <TouchableOpacity className="items-center mt-10 bg-blue-600 p-5 rounded-[30px]">
+              <Text className="text-white font-bold text-[15px]">FACEBOOK</Text>
+            </TouchableOpacity>
+            <TouchableOpacity className="items-center mt-10 bg-gray-200 p-5 rounded-[30px]">
+              <Text className="text-black font-bold text-[15px]">GOOGLE</Text>
+            </TouchableOpacity>
+          </View>
+
+          <View className="mt-10 items-center">
+            <View>
+              <Text className="flex">Don't have an account?</Text>
+              <TouchableOpacity
+                onPress={() => navigation.navigate("signin")}
+                className="mt-3 items-center"
+              >
+                <Text className="text-blue-400 font-bold">SIGN IN</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
         </View>
       </View>
-    </KeyboardAvoidingView>
+    </View>
   );
-};
+}
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    paddingTop: 20,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-    bottom: 50,
+    // // flex: 1,
+    // backgroundColor: "#fff",
+    // alignItems: "center",
+    // justifyContent: "center",
+    // bottom: 50,
   },
   error: {
     marginTop: 10,
     color: "red",
   },
 });
-
-export default SignUpScreen;
